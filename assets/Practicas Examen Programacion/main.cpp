398153157eab6aa62a219bb593a348b107435eba
Aldo Cañiza <aldocaniza@gmail.com>
2024-10-29 12:16:58 -0300

    prueba c++

---
+++ assets/Practicas Examen Programacion/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+//Declaracion de Variables
+int filas = 5;
+int columnas = 5;
+
+int main(){
+    cout << "Cod \t Cant \t Precio \t Importe \t Acumul " << endl;
+    
+    
+    return 0;
+    }
